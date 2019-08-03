bc12d0b41672e85205d9b216ed95c45d3fb5da49
1728999504 <shaog@wit.edu>
2019-08-03 17:07:33 -0400

    Update Apply_Filter.m

--- Apply_Filter.m
+++ Apply_Filter.m
@@ -9,3 +9,20 @@ function output = Apply_Filter(img, filter)
     
     output = filtered_img;
 end
+
+
+
+%Shawn's filter
+%function output = Apply_Filter(img, filter)
+%    u_filter = uint8(filter);
+    
+%    filtered_img = zeros(size(img));
+    
+%    for i = 1:size(img, 1)
+%        for q = 1:size(img, 2)
+%            filtered_img(i, q) = img(i, q) * u_filter(i, q);
+%        end
+%    end
+    
+%    output = filtered_img;
+%end
